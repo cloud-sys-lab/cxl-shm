b4281281b8d5dec1c0c1025dbc71f0dc4e76cb7a
xhl <2637808037@qq.com>
2024-08-12 17:08:57 +0000

    good

--- test/test_api.cpp
+++ test/test_api.cpp
@@ -16,6 +16,9 @@ size_t length;
 int shm_id;
 int counter = 1;
 
+int DATA_SIZE_BLOCK = 128;
+int DATA_SIZE_MESSAGE = 128;
+
 void consumer(uint64_t queue_offset, std::promise<uint64_t> &offset)
 {
     //sleep(1);
@@ -106,33 +109,77 @@ int main()
 
     CHECK_BODY("data transfer") {
         CXLRef r1 = shm.cxl_malloc(100, 0);
+        void* start = shm.get_start();
         uint64_t queue_offset1 = shm.create_msg_queue(2);
+        
+        #ifdef THREAD2
         uint64_t queue_offset2 = shm.create_msg_queue(4);
-        shm.sent_to(queue_offset1, r1);
-        // shm.sent_to(queue_offset1, r1);
-        // shm.sent_to(queue_offset2, r1);
-        shm.sent_to(queue_offset2, r1);
+        #endif
+        std::vector<CXLRef> block_vec;
+        for (int i = 0; i < counter; i++) {
+            CXLRef r = shm.cxl_malloc_wrc(DATA_SIZE_BLOCK, 0);
+            block_vec.push_back(r);
+        }
+
+        for (int i = 0; i < counter; i++) {
+            CXLRef r1 = block_vec[i];
+            uint64_t obj_offset = r1.data;
+            CXLObj* cxl_obj = (CXLObj*)get_data_at_addr(start, obj_offset);
+        
+            while (cxl_obj->reader_count != 0 && cxl_obj->writer_count != 0) {
+                
+            }
+            if (cxl_obj->reader_count == 0 && cxl_obj->writer_count == 0) {
+                cxl_obj->writer_count++;
+                RootRef* tbr1 = (RootRef*) get_data_at_addr(start, r1.tbr);
+                tbr1->ref_cnt++;
+                cxl_obj->str_content = "bbb";
+                cxl_obj->writer_count--;
+            }
+
+
+            shm.sent_to(queue_offset1, r1);
+            // shm.sent_to(queue_offset1, r1);
+            #ifdef THREAD2
+            // shm.sent_to(queue_offset2, r1);
+            shm.sent_to(queue_offset2, r1);
+            #endif
+            // bool send_res1    = shm.sent_to(queue_offset1, r1);
+            // #ifdef THREAD2
+            // bool send_res2    = shm.sent_to(queue_offset2, r1);
+            // #endif
+            // #ifdef THREAD3
+            // bool send_res3    = shm.sent_to(queue_offset3, r1);
+            // #endif
+        }
+
+
 
         std::promise<uint64_t> offset_1;
         std::promise<std::chrono::time_point<std::chrono::system_clock>> t_receiver1;
+        std::thread t1(consumer_wrc, queue_offset1, std::ref(offset_1), std::ref(t_receiver1));
+        sleep(1);
+        #ifdef THREAD2
 
         std::promise<uint64_t> offset_2;
         std::promise<std::chrono::time_point<std::chrono::system_clock>> t_receiver2;
-        
+        std::thread t2(consumer_wrc, queue_offset2, std::ref(offset_2), std::ref(t_receiver2));
+        #endif
         //std::cout << "test_warpper thread1 queue_offset1:" << queue_offset1 << std::endl;
-        std::thread t1(consumer_wrc, queue_offset1, std::ref(offset_1), std::ref(t_receiver1));
         
         //std::thread t1(consumer, queue_offset1, std::ref(offset_1));
         
-        sleep(1);
         //std::thread t2(consumer_wrc, queue_offset2, std::ref(offset_2));
 
-        std::thread t2(consumer_wrc, queue_offset2, std::ref(offset_2), std::ref(t_receiver2));
         t1.join();
-        
+        auto t_real_receive1 = t_receiver1.get_future().get();
+        auto t_end = t_real_receive1;
         sleep(1);
+        #ifdef THREAD2
         t2.join();
-
+        auto t_real_receive2 = t_receiver2.get_future().get();
+        t_end = t_end > t_real_receive2 ? t_end : t_real_receive2;
+        #endif
         // sleep(1);
         
         std::cout  << "1111: status"  << std::endl;
